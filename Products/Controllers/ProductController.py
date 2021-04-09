80b16bd85d327880633dcff841c826896cf32ef2
nadermedhet148 <nadermedhet142@gmail.com>
2021-04-09 11:00:02 +0200

    [FEAT] getProduct end-point

--- Products/Controllers/ProductController.py
+++ Products/Controllers/ProductController.py
@@ -10,12 +10,27 @@ ProductController = Blueprint('ProductController', __name__,
                         template_folder='templates')
 
 @ProductController.route('/',methods=['POST'])
-def createUser():
+def createProduct():
     body = request.get_json()
-    product = Product(name=body.get('name'),price=body.get('price'))
+    product = Product(
+        name=body.get('name'),
+        price=body.get('price'),
+        quantity=body.get('quantity')
+        )
     db.session.add(product)
     db.session.commit()
     return {
         "success": True,
+        'data' : product.toDict()
+    }
+
+
+
+@ProductController.route('/<productId>',methods=['GET'])
+def getProduct(productId):
+    product = Product.query.get(productId)
+    return {
+        "success": True,
+        "data": product.toDict()
     }
 
--- Products/models/Product.py
+++ Products/models/Product.py
@@ -4,3 +4,12 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=False, nullable=False)
     price = db.Column(db.Integer, unique=False, nullable=False)
+    quantity = db.Column(db.Integer, unique=False, nullable=False)
+
+    def toDict(self):
+        return{
+            "id" : self.id,
+            "name" : self.name, 
+            "price" : self.price,
+            "quantity" : self.quantity,
+        }
