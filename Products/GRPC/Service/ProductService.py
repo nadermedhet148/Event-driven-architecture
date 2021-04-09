5f136630bf07334c90ca700023481b1d7294119f
nadermedhet148 <nadermedhet142@gmail.com>
2021-04-09 20:18:06 +0200

    [feat] Products/ add GRPC for

---
+++ Products/GRPC/Service/ProductService.py
@@ -0,0 +1,17 @@
+from GRPC.protosImplantation.products_pb2_grpc import ProductsServicer
+import GRPC.protosImplantation.products_pb2 as Products_types_pb2
+
+import models.Product as ProductModel
+
+class ProductService(ProductsServicer):
+
+    def GetProduct(self, request, context):
+        product = ProductModel.Product.query.get(request.id)
+        print(request.id)
+        return Products_types_pb2.Product(
+            name=product.name,
+            id=product.id ,
+            price=product.price ,
+            quantity=product.quantity
+            )
+        
---
+++ Products/GRPCServer.py
@@ -0,0 +1,23 @@
+import asyncio
+import time
+import math
+import logging
+from typing import AsyncIterable, Iterable
+
+import grpc
+
+from GRPC.protosImplantation.products_pb2_grpc import add_ProductsServicer_to_server
+from GRPC.Service.ProductService import ProductService
+
+async def serve() -> None:
+    server = grpc.aio.server()
+    add_ProductsServicer_to_server(
+        ProductService(), server)
+    server.add_insecure_port('[::]:50052')
+    await server.start()
+    await server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(serve())
