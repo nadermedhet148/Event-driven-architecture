d5c3c48f2c3829615306b3b7fb9d09a680888715
nadermedhet148 <nadermedhet142@gmail.com>
2021-04-17 20:54:15 +0200

    [feat] add order grpc Product service

--- Orders/GRPC/Requests.py
+++ Orders/GRPC/Requests.py
@@ -1,5 +1,7 @@
 import GRPC.protosImplantation.users_pb2_grpc as users_service
 import GRPC.protosImplantation.users_types_pb2 as users_messages
+import GRPC.protosImplantation.products_pb2_grpc as products_service
+import GRPC.protosImplantation.products_pb2 as products_messages
 import grpc
 from functools import partial
 
@@ -9,3 +11,9 @@ def getUser(userId):
     stub = users_service.UsersStub(channel)
     user = stub.GetUser(users_messages.User(id=userId))
     return user
+
+def getProduct(userId):
+    channel = grpc.insecure_channel('localhost:50052')
+    stub = products_service.ProductsStub(channel)
+    product = stub.GetProduct(products_messages.Product(id=userId))
+    return product
